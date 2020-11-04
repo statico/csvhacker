a06e40039269c0678366aec8543df40dab37c601
Ian Langworth <ian@langworth.com>
2020-11-04 15:18:14 -0800

    Add Recoil

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import { AppProps } from "next/app"
 import React from "react"
+import { RecoilRoot } from "recoil"
 
 import "../styles.css"
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,32 @@
-const Page = () => <h1 className="text-4xl">hello</h1>
+import { atom, useRecoilValue } from "recoil"
+
+const inputState = atom<null | any[][]>({
+  key: "input",
+  default: new Array(20)
+    .fill(0)
+    .map((i) =>
+      new Array(5)
+        .fill(0)
+        .map((j) => Math.floor(Math.random() * 1e15).toString(36))
+    ),
+})
+
+const Page = () => {
+  const input = useRecoilValue(inputState)
+  return (
+    <div>
+      <h1 className="text-xl">Data</h1>
+      <table>
+        {input.map((row, i) => (
+          <tr key={i}>
+            {row.map((cell, j) => (
+              <td key={j}>{cell}</td>
+            ))}
+          </tr>
+        ))}
+      </table>
+    </div>
+  )
+}
 
 export default Page
