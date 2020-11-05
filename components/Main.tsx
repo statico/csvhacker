4c90fb426d5f0fcee3b4927655c8696a71c6833a
Ian Langworth <ian@langworth.com>
2020-11-05 14:31:06 -0800

    Basic draggability boilerplate

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,11 +1,20 @@
 import { atom, selector, useRecoilValue } from "recoil"
 import Papa from "papaparse"
+import {
+  DragDropContext,
+  Draggable,
+  DraggableProvided,
+  DraggableProvidedDraggableProps,
+  DraggableStateSnapshot,
+  Droppable,
+  OnDragEndResponder,
+} from "react-beautiful-dnd"
 
 const inputState = selector<null | any[][]>({
   key: "input",
   get: ({ get }) => {
     return new Promise((resolve, reject) => {
-      Papa.parse("http://localhost:8080/100.csv", {
+      Papa.parse("http://localhost:8080/10k.csv", {
         download: true,
         error: reject,
         worker: true,
@@ -49,22 +58,96 @@ const outputState = selector<any[][]>({
   },
 })
 
-const Main = () => {
+const getStyle = (
+  style: DraggableProvidedDraggableProps["style"],
+  snapshot: DraggableStateSnapshot
+): any => {
+  if (!snapshot.isDropAnimating) {
+    return style
+  }
+  return {
+    ...style,
+    transitionDuration: `0.001s`,
+  }
+}
+
+const Filters = () => {
   const filters = useRecoilValue(filterState)
+  const onDragEnd: OnDragEndResponder = ({
+    draggableId,
+    source,
+    destination,
+  }) => {
+    console.log("XXX", "onDragEnd", draggableId, source, destination)
+  }
+
+  return (
+    <div className="flex flex-row">
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="toolbox" isDropDisabled={true}>
+          {(provided, snapshot) => (
+            <div ref={provided.innerRef} className="mr-5">
+              {["head", "tail"].map((key, i) => (
+                <Draggable draggableId={key} index={i} key={key}>
+                  {(
+                    provided: DraggableProvided,
+                    snapshot: DraggableStateSnapshot
+                  ) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      className="select-none bg-white border border-gray-800 b-2 mb-2 p-2 block"
+                      style={getStyle(provided.draggableProps.style, snapshot)}
+                    >
+                      {key}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+            </div>
+          )}
+        </Droppable>
+
+        <Droppable droppableId="filters">
+          {(provided, snapshot) => (
+            <div ref={provided.innerRef}>
+              {filters.map((filter, i) => (
+                <Draggable draggableId={String(i)} index={i} key={i}>
+                  {(
+                    provided: DraggableProvided,
+                    snapshot: DraggableStateSnapshot
+                  ) => (
+                    <div
+                      ref={provided.innerRef}
+                      key={i}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      className="bg-white border border-gray-800 b-2 mb-2 p-2 block"
+                      style={getStyle(provided.draggableProps.style, snapshot)}
+                    >
+                      {i}: {JSON.stringify(filter)}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  )
+}
+
+const Main = () => {
   const output = useRecoilValue(outputState)
 
   return (
     <div className="p-2 flex flex-row">
       <div className="mr-5">
         <h1 className="text-xl">Filters</h1>
-        {filters.map((filter, i) => (
-          <div
-            key={i}
-            className="border border-gray-800 b-2 mb-2 p-2 inline-block"
-          >
-            {i}: {JSON.stringify(filter)}
-          </div>
-        ))}
+        <Filters />
       </div>
       <div>
         <h1 className="text-xl">Data</h1>
