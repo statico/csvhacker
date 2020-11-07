078262f3f26e7fb474c18d92bd02a5044516845f
Ian Langworth <ian@langworth.com>
2020-11-06 20:56:55 -0800

    Added preliminary favicon from https://www.iconfinder.com/iconsets/cad-database-presentation-spreadsheet-vector-fil-2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import dynamic from "next/dynamic"
 
 // We don't need or want SSR, which messes up Recoil somehow.
@@ -5,7 +6,14 @@ import dynamic from "next/dynamic"
 const MainNoSSR = dynamic(() => import("../components/Main"), { ssr: false })
 
 const Page = () => {
-  return <MainNoSSR />
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
+      <MainNoSSR />
+    </>
+  )
 }
 
 export default Page
