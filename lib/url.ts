a24b21178b89005762023d37ef6bbd395c14e3b9
Ian Langworth <ian@langworth.com>
2020-11-13 12:11:55 -0800

    Use rison for more compact URL state

--- lib/url.ts
+++ lib/url.ts
@@ -1,7 +1,9 @@
+import rison from "rison-node"
+
 export const getUrlState = (): any => {
   if (!process.browser || !document.location.hash) return {}
   try {
-    return JSON.parse(decodeURI(document.location.hash.substr(1))) || {}
+    return rison.decode(document.location.hash.substr(1)) || {}
   } catch (err) {
     console.error("Could not parse state from URL")
     return {}
@@ -11,6 +13,6 @@ export const getUrlState = (): any => {
 export const setUrlState = (state: any): void => {
   if (!process.browser) return
   const url = new URL(document.location.href)
-  url.hash = JSON.stringify(state)
+  url.hash = Object.keys(state).length ? rison.encode(state) : ""
   history.pushState(null, "", url.toString())
 }
