3ba0cfa3033425edc07e438b4df299fc27c89bdf
Ian Langworth <ian@langworth.com>
2020-11-17 11:35:36 -0800

    Remove some parens from the URL

--- lib/url.ts
+++ lib/url.ts
@@ -1,9 +1,11 @@
 import rison from "rison-node"
 
+// Using encode_object/decode_object instead of encode/decode saves some parens.
+
 export const getUrlState = (): any => {
   if (!process.browser || !document.location.hash) return {}
   try {
-    return rison.decode(document.location.hash.substr(1)) || {}
+    return rison.decode_object(document.location.hash.substr(1)) || {}
   } catch (err) {
     console.error("Could not parse state from URL")
     return {}
@@ -13,6 +15,6 @@ export const getUrlState = (): any => {
 export const setUrlState = (state: any): void => {
   if (!process.browser) return
   const url = new URL(document.location.href)
-  url.hash = Object.keys(state).length ? rison.encode(state) : ""
+  url.hash = Object.keys(state).length ? rison.encode_object(state) : ""
   history.pushState(null, "", url.toString())
 }
